1e66a8eb33a861735096cbcc465786f06f131173
qxzhoupku <121421249+qxzhoupku@users.noreply.github.com>
2023-09-26 12:40:51 +0800

    update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import sys
 import multiprocessing as mp
 
 # list_M = [0.1, 0.2, 0.3, 0.4]
-list_M = [0.1, 0.2]
+list_M = [0.1]
 # list_P_pump = [10, 20, 100]
-list_P_pump = [10, 20]
+list_P_pump = [10]
 
 pool = []
 for M in list_M:
     for P_pump in list_P_pump:
-        pool.append(mp.Process(target=os.system, args=(f"python main.py {M} {P_pump}",)))
+        pool.append(mp.Process(target=os.system, args=(f"python simulation.py {M} {P_pump}",)))
 
 if __name__ == "__main__":
     for p in pool:
